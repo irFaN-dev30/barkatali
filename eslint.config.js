70e96f847a74b6309a0ce254c3345b60705f7110
belabela1491 <belabela1491@gmail.com>
2026-04-16 16:23:08 +0000

    uiux

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,53 @@
-import js from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import nextPlugin from '@next/eslint-plugin-next';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi"] },
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    // Next.js plugin configuration
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      '@next/next': nextPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
     },
   },
-  eslintPluginPrettier,
-);
+  {
+    // React plugin settings
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    ...pluginReactConfig,
+    rules: {
+        ...pluginReactConfig.rules,
+        'react/react-in-jsx-scope': 'off', // Not needed in Next.js 17+
+        'react/prop-types': 'off' // Optional: if you are using TypeScript for props
+    }
+  },
+  {
+    // Prettier configuration
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'warn',
+    },
+  },
+  {
+    // Ignores node_modules and the .next build directory
+    ignores: ['node_modules/', '.next/'],
+  },
+];
